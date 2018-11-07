ba5e811f7e58e538d05b851ccdde66f9ecfedab3
Jan Cavel <caveljan@gmail.com>
2018-11-07 19:17:05 +0200

    dev: stringified state

--- src/ts/source/core/stateShareImage.ts
+++ src/ts/source/core/stateShareImage.ts
@@ -8,27 +8,77 @@ import { defaultBaseImage } from './defaultBaseImage';
  * @param stateObject   Application State Object
  */
 function stateShareImage(stateObject: Object) {
+    // to implement algorithms for mapping text-to-image
+    // LSB - least significant bit
+    // MSB - most significant bit
+    // DFT - discrete fourier transform
+    // DCT - discrete cosine transform
+    // DWT - discrete wavelet transform
+    // RSQ - random sequence generator
+    // CM  - chaotic map
+    // ECC - error correction code
+    // SPIHTR - set partitioning in hierarchical tree references
+
     let baseImage = '';
+    const stateString = JSON.stringify(stateObject);
+    console.log('stateString', stateString);
 
     const domainImageMetaTag = document.querySelector('meta[property="state-share-image"]');
     const domainImageSrc = domainImageMetaTag ? domainImageMetaTag.getAttribute('content') : '';
 
     if (domainImageSrc) {
-        // read domainImage from domainImageSrc
-        baseImage = 'readImage';
+        // const image = getImageDataURL(domainImageSrc);
     } else {
         baseImage = defaultBaseImage;
     }
 
-
-    // console.log('baseImage', baseImage);
-    // console.log('stateObject', stateObject);
+    console.log('baseImage', baseImage);
 
     const stateImage = baseImage;
-    // console.log('stateImage', stateImage);
 
     return stateImage;
 }
+
+
+
+// function getImageDataURL(url) {
+//     let data, canvas, ctx;
+//     let img = new Image();
+
+//     img.onload = () => {
+//         canvas = document.createElement("canvas");
+//         canvas.width = img.width;
+//         canvas.height = img.height;
+
+//         ctx = canvas.getContext("2d");
+//         ctx.drawImage(img, 0, 0);
+
+//         try {
+//             data = canvas.toDataURL();
+//             // blob = dataURIToBlob(data);
+//             // console.log(data);
+//         } catch(e) {
+//             console.log(e);
+//         }
+//     };
+
+//     img.src = url;
+
+//     return data;
+// };
+
+
+// function dataURIToBlob (dataURI) {
+//     var byteString = atob(dataURI.split(',')[1]);
+//     var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+//     var ab = [];
+
+//     for (var i = 0; i < byteString.length; i++)
+//         ab.push(byteString.charCodeAt(i));
+
+//     return new Blob([new Uint8Array(ab)], { type: mimeString });
+// };
+
 
 
 
@@ -39,6 +89,10 @@ let state = {
         theme: 'night'
     }
 }
-
 let shareImage = stateShareImage(state);
-console.log(shareImage);
+let body = document.body;
+let image = new Image();
+image.src = shareImage;
+body.appendChild(image);
+
+// console.log('shareImage', shareImage);
