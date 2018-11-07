1b37c04dd3294f5f9dad8d9444363f96fcdc393d
Jan Cavel <caveljan@gmail.com>
2018-11-07 08:09:54 +0200

    dev: litelement

--- src/ts/source/components/state-share-image/state-share-image.ts
+++ src/ts/source/components/state-share-image/state-share-image.ts
@@ -1,7 +1,24 @@
-class StateShareImage extends HTMLElement {
+import {LitElement, html} from '@polymer/lit-element';
+
+
+
+class StateShareImage extends LitElement {
+    static get properties() {
+        return {
+        };
+    }
+
     constructor() {
         super();
     }
+
+    render() {
+        return html`
+            state-share-image
+        `;
+    }
+
 }
+
 
 customElements.define('state-share-image', StateShareImage);
