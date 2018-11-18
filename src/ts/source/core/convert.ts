29d5ef522ce3247c395d74aea919c2c5ec4addd2
Jan Cavel <caveljan@gmail.com>
2018-11-18 14:01:14 +0200

    dev: core > convert cleanup

--- src/ts/source/core/convert.ts
+++ src/ts/source/core/convert.ts
@@ -1,6 +1,11 @@
 interface IConvert {
     toBinary(character: string): string;
+
     fromBinary(binary: string): string;
+
+    /**
+     * binary: string gives padStart error from TypeScript
+     */
     zeroPad(binary: any): string;
 }
 
@@ -8,34 +13,30 @@ interface IConvert {
 
 export const convert: IConvert = {
     /**
-     * Converts a given character to a binary string and pads it with 0 (16 bits).
-     * e.g., 'A' to '0000000001000001'
+     * Converts a given character to a binary string and pads it with 0 (32 bits).
+     * e.g., 'A' to '00000000000000000000000001000001'
      *
      * @param character
      */
     toBinary(character) {
-        // return convert.zeroPad(character.charCodeAt(0).toString(2));
         return convert.zeroPad(character.codePointAt(0).toString(2));
     },
 
 
     /**
      * Converts from binary string to string character.
-     * e.g., '0000000001000001' to 'A'
+     * e.g., '00000000000000000000000001000001' to 'A'
      *
      * @param binary
      */
     fromBinary(binary) {
-        // return String.fromCharCode(parseInt(binary, 2));
         return String.fromCodePoint(parseInt(binary, 2));
     },
 
 
      /**
-     * Pads string with 0 (16 bits).
-     * e.g., '1000001' to '0000000001000001'
-     *
-     * binary: string gives padStart error from TypeScript
+     * Pads string with 0 (32 bits).
+     * e.g., '10000000000000001' to '00000000000000010000000000000001'
      *
      * @param binary
      */
