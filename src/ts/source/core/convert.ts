f4cdef6f4276702f117b5bc57f17348bc3c49a4a
Jan Cavel <caveljan@gmail.com>
2018-11-18 12:14:38 +0200

    dev: convert interface

--- src/ts/source/core/convert.ts
+++ src/ts/source/core/convert.ts
@@ -1,38 +1,42 @@
-/**
- * Converts a given character to a binary string and pads it with 0 (8 bits).
- * e.g., 'A' to '01000001'
- *
- * Note:
- * Characters spanning more than 8 bits, as is ț or ș,
- * are returned with all their bits.
- *
- * @returns {Number}
- */
-export var convert = {
+interface IConvert {
+    toBinary(character: string);
+    fromBinary(binary: string);
+    zeroPad(binary: any);
+}
+
+
+export const convert: IConvert = {
+    /**
+     * Converts a given character to a binary string and pads it with 0 (16 bits).
+     * e.g., 'A' to '0000000001000001'
+     *
+     * @param character
+     */
     toBinary(character: String) {
         return character.replace(/[\s\S]/g, (character) => {
             return convert.zeroPad(character.charCodeAt(0).toString(2));
         });
     },
-    fromBinary(str: String) {
-        // https://ourcodeworld.com/articles/read/380/how-to-convert-a-binary-string-into-a-readable-string-and-vice-versa-with-javascript
 
-        str = str.replace(/\s+/g, '');
-
-        // Pretty (correct) print binary (add a space every 16 characters)
-        str = str.match(/.{1,16}/g).join(" ");
-
-        var newBinary = str.split(" ");
-        var binaryCode = [];
-
-        for (let i = 0; i < newBinary.length; i++) {
-            binaryCode.push(String.fromCharCode(parseInt(newBinary[i], 2)));
-        }
-
-        return binaryCode.join("");
+    /**
+     * Converts from binary string to string character.
+     * e.g., '0000000001000001' to 'A'
+     *
+     * @param binary
+     */
+    fromBinary(binary: string) {
+        return String.fromCharCode(parseInt(binary, 2))
     },
+
+     /**
+     * Pads string with 0 (16 bits).
+     * e.g., '1000001' to '0000000001000001'
+     *
+     * binary: string gives padStart error from TypeScript
+     *
+     * @param binary
+     */
     zeroPad(binary: any) {
-        // binary: String gives padStart error from TypeScript
         return binary.padStart(16, '0');
     }
 };
